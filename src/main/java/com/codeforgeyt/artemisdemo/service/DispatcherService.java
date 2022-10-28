4ddd2cf453fc060c432f4742f4c9c69e82c5b02a
Mahamadi BELEM <mahamadibel@gmail.com>
2022-10-28 15:11:42 +0000

    second commit

---
+++ src/main/java/com/codeforgeyt/artemisdemo/service/DispatcherService.java
@@ -0,0 +1,25 @@
+package com.codeforgeyt.artemisdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+/****************************************
+ * 
+ * Author: Mahamadi BELEM
+ ****************************************/
+
+@Service
+public class DispatcherService {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Value("${jms.queue}")
+    String jmsQueue;
+
+    public void sendMessage(String message){
+        jmsTemplate.convertAndSend(jmsQueue, message);
+    }
+}
